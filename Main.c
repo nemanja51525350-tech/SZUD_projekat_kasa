358eaf6a47b4815fbdadaf8c3f9901f0ebebda38
nemanja51525350-tech <nemanja51525350@gmail.com>
2025-12-21 18:56:07 +0100

    Kreiranje main funkcije u Main.c

--- Main.c
+++ Main.c
@@ -8,3 +8,18 @@ typedef struct {
     int kolicina;
     float cena;
 } Artikal;
+
+int main() {
+    FILE *f;
+    Artikal a[MAX];
+    int n, i;
+    float ukupno = 0;
+    char kes;
+    float uplata = 0, kusur = 0;
+    time_t t;
+
+    f = fopen("Fisk.txt", "a");
+    if (f == NULL) {
+        printf("Greska pri otvaranju fajla!\n");
+        return 1;
+    }
